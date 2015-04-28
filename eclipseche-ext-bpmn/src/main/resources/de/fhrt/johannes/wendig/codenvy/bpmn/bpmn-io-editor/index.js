2a10c569c33160d0748ea7cec11983863d63420c
things4it <things4it@gmail.com>
2015-04-28 13:25:31 +0200

    - bugfix at bpmnio index.js

--- eclipseche-ext-bpmn/src/main/resources/de/fhrt/johannes/wendig/codenvy/bpmn/bpmn-io-editor/index.js
+++ eclipseche-ext-bpmn/src/main/resources/de/fhrt/johannes/wendig/codenvy/bpmn/bpmn-io-editor/index.js
@@ -1,9 +1,10 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // BEGIN COPY BLOG (copy this to the top of the generated .js document)
 
 var bpmnIo_fkt_createNewModeler;
 
 // END COPY BLOG
+
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
 bpmnIo_fkt_createNewModeler = function(wrapperId){
 	
