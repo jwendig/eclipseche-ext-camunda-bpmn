5fc26365b30ebffdda1935915f8f4d5362c25448
things4it <things4it@gmail.com>
2015-09-17 10:04:55 +0200

    - licence header added

--- src/main/java/de/fhrt/codenvy/bpmn/editor/widget/diagram/BpmnEditorSourceWidget.java
+++ src/main/java/de/fhrt/codenvy/bpmn/editor/widget/diagram/BpmnEditorSourceWidget.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
 package de.fhrt.codenvy.bpmn.editor.widget.diagram;
 
 import edu.stanford.bmir.gwtcodemirror.client.GWTCodeMirror;
@@ -15,5 +25,4 @@ public class BpmnEditorSourceWidget extends GWTCodeMirror {
 											cm.CodeMirror.refresh();
 										}
 										}-*/;
-
 }
