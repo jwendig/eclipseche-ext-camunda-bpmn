4b9c4b94acf8d50281d59d6aa382f4524ce39081
things4it <things4it@gmail.com>
2015-09-23 11:02:38 +0200

    - fields for properties of multiInstanceLoopCharacteristics added

--- src/main/java/de/fhrt/codenvy/bpmn/part/bpmnProperties/widgets/elements/reusableTabs/multiinstance/TabMultiInstanceView.java
+++ src/main/java/de/fhrt/codenvy/bpmn/part/bpmnProperties/widgets/elements/reusableTabs/multiinstance/TabMultiInstanceView.java
@@ -14,6 +14,7 @@ package de.fhrt.codenvy.bpmn.part.bpmnProperties.widgets.elements.reusableTabs.m
 import org.eclipse.che.ide.util.loging.Log;
 
 import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.TextBox;
 
 import de.fhrt.codenvy.bpmn.part.bpmnProperties.BpmnPropertiesView;
 import de.fhrt.codenvy.bpmn.part.bpmnProperties.widgets.AbstractBpmnPropertiesTabWidget;
@@ -21,6 +22,12 @@ import de.fhrt.codenvy.bpmn.part.bpmnProperties.widgets.AbstractBpmnPropertiesTa
 public class TabMultiInstanceView extends AbstractBpmnPropertiesTabWidget {
 	private CheckBox cbIsLoop;
 	private CheckBox cbMultiInstance;
+
+	private TextBox tbMultiInstanceLoopCardinality;
+	private CheckBox cbMultiInstanceIsSequential;
+	private TextBox tbMultiInstanceCollection;
+	private TextBox tbMultiInstanceElementVariable;
+	private TextBox tbMultiInstanceCompletionCondition;
 
 	public TabMultiInstanceView(String tabName,
 			BpmnPropertiesView.CurrentJsoAccess jsoAccess) {
@@ -31,13 +38,24 @@ public class TabMultiInstanceView extends AbstractBpmnPropertiesTabWidget {
 	@Override
 	public void initContent() {
 		Log.info(TabMultiInstanceView.class, "initContent");
-		getGridTabContent().resize(2, 2);
+		getGridTabContent().resize(7, 2);
 
 		getGridTabContent().setText(0, 0, "Is Loop:");
 		getGridTabContent().setText(1, 0, "Is Multi Instance:");
+		getGridTabContent().setText(2, 0, "Loop Cardinality:");
+		getGridTabContent().setText(3, 0, "Is Sequential:");
+		getGridTabContent().setText(4, 0, "Collection:");
+		getGridTabContent().setText(5, 0, "Element Variable:");
+		getGridTabContent().setText(6, 0, "Completion Condition:");
 
 		getGridTabContent().setWidget(0, 1, cbIsLoop);
 		getGridTabContent().setWidget(1, 1, cbMultiInstance);
+		getGridTabContent().setWidget(2, 1, tbMultiInstanceLoopCardinality);
+		getGridTabContent().setWidget(3, 1, cbMultiInstanceIsSequential);
+		getGridTabContent().setWidget(4, 1, tbMultiInstanceCollection);
+		getGridTabContent().setWidget(5, 1, tbMultiInstanceElementVariable);
+		getGridTabContent().setWidget(6, 1, tbMultiInstanceCompletionCondition);
+		
 	}
 
 	@Override
@@ -49,6 +67,21 @@ public class TabMultiInstanceView extends AbstractBpmnPropertiesTabWidget {
 		cbMultiInstance = new CheckBox();
 		cbMultiInstance.setWidth("100%");
 
+		tbMultiInstanceLoopCardinality = new TextBox();
+		tbMultiInstanceLoopCardinality.setWidth("100%");
+
+		cbMultiInstanceIsSequential = new CheckBox();
+		cbMultiInstanceIsSequential.setWidth("100%");
+
+		tbMultiInstanceCollection = new TextBox();
+		tbMultiInstanceCollection.setWidth("100%");
+
+		tbMultiInstanceElementVariable = new TextBox();
+		tbMultiInstanceElementVariable.setWidth("100%");
+
+		tbMultiInstanceCompletionCondition = new TextBox();
+		tbMultiInstanceCompletionCondition.setWidth("100%");
+
 	}
 
 	public CheckBox getCbIsLoop() {
@@ -57,6 +90,26 @@ public class TabMultiInstanceView extends AbstractBpmnPropertiesTabWidget {
 
 	public CheckBox getCbMultiInstance() {
 		return cbMultiInstance;
+	}
+
+	public TextBox getTbMultiInstanceLoopCardinality() {
+		return tbMultiInstanceLoopCardinality;
+	}
+
+	public CheckBox getCbMultiInstanceIsSequential() {
+		return cbMultiInstanceIsSequential;
+	}
+
+	public TextBox getTbMultiInstanceCollection() {
+		return tbMultiInstanceCollection;
+	}
+
+	public TextBox getTbMultiInstanceElementVariable() {
+		return tbMultiInstanceElementVariable;
+	}
+
+	public TextBox getTbMultiInstanceCompletionCondition() {
+		return tbMultiInstanceCompletionCondition;
 	}
 
 }
